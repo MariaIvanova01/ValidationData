1c8fc9222f680bd58abb19d00123e509db32f951
MariaIvanova01 <mimi200801@abv.bg>
2022-07-08 19:27:22 +0300

    Using validations, classes and encapsulaton.

--- ValidationData/src/com/company/Main.java
+++ ValidationData/src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+
+        int countInput = Integer.parseInt(scan.nextLine());
+
+        List<Person> workers = new ArrayList<>();
+        for (int i = 0; i < countInput; i++) {
+            String[] personInfo = scan.nextLine().split(" ");
+
+            String firstName = personInfo[0];
+            String lastName = personInfo[1];
+            int age = Integer.parseInt(personInfo[2]);
+            double salary = Double.parseDouble(personInfo[3]);
+
+            workers.add(new Person(firstName,lastName,age,salary));
+        }
+
+        double bonus = Double.parseDouble(scan.nextLine());
+
+        for (Person worker : workers) {
+            worker.increaseSalary(bonus);
+            System.out.print(worker);
+        }
     }
 }
